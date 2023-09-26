e97d32ea0d8f362207403e63344aa1fb4f15d907
Samin Amanat <amanat361@gmail.com>
2023-09-26 00:12:14 -0700

    moved search state to url params

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,11 +1,22 @@
-"use client"
+"use client";
 import { MagnifyingGlassIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { useRef, useEffect, useState } from "react";
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
 
 export default function Search({}) {
   const [isFocused, setIsFocused] = useState(false);
-  const [query, setQuery] = useState("");
   const inputRef = useRef<HTMLInputElement>(null);
+
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const updateQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const query = e.target.value;
+    const params = new URLSearchParams(searchParams);
+    params.set("q", query);
+    router.push(pathname + "?" + params.toString());
+  };
 
   useEffect(() => {
     const handleKeydown = (e: KeyboardEvent) => {
@@ -26,21 +37,24 @@ export default function Search({}) {
   }, []);
 
   return (
-    <div className={`flex flex-1 items-center gap-2 rounded-lg border-[1px] p-3 transition-colors ${isFocused ? "border-gray-800" : "border-gray-200"}`} onClick={() => inputRef.current?.focus()}>
+    <div
+      className={`flex flex-1 items-center gap-2 rounded-lg border-[1px] p-3 transition-colors ${
+        isFocused ? "border-gray-800" : "border-gray-200"
+      }`}
+      onClick={() => inputRef.current?.focus()}
+    >
       <MagnifyingGlassIcon className="h-4 w-4 text-gray-500" />
       <input
-        className="flex-1 border-none p-0 text-sm placeholder-gray-400 text-gray-800 focus:border-none focus:ring-0"
+        className="flex-1 border-none p-0 text-sm text-gray-800 placeholder-gray-400 focus:border-none focus:ring-0"
         placeholder="Search or jump to"
         ref={inputRef}
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
+        onChange={updateQuery}
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
       />
       <kbd className="rounded-lg bg-gray-100 px-2 py-1 text-xs text-gray-500">
         ctrl + k
       </kbd>
-      <XMarkIcon className="h-4 w-4 text-gray-500 hover:text-black" onClick={() => setQuery("")} />
     </div>
   );
 }
---
+++ src/utils/testparams.js
@@ -0,0 +1,3 @@
+const params = new URLSearchParams({'hello':"go od////     +++dfgjhdjkghj444bye"}).toString()
+
+console.log(params)
